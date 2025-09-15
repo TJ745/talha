aeb537d0917c8d527c21e95c7fd216f12019c47f
TJ745 <talha_mughalz@hotmail.com>
2025-09-15 15:25:10 +0300

    Fixed About and Contact Pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 import Resume from "@/components/Resume";
+import { motion } from "framer-motion";
 
 interface LocaleProps {
   locale: "en" | "ar";
@@ -23,8 +24,36 @@ function About({ locale }: LocaleProps) {
           </h3>
           <p>{t("subtitle")}</p>
         </div>
-        <div className="flex justify-center items-center gap-x-[75px]">
-          <div className="max-w-5xl flex flex-col justify-center">
+        <div className="max-w-7xl grid items-center grid-cols-1 lg:grid-cols-2 mb-8 gap-8 lg:gap-0">
+          <motion.div
+            className="mx-auto hidden lg:block"
+            initial={{ opacity: 0, x: -50 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: false }}
+          >
+            <Image
+              src={"/images/About.jpeg"}
+              alt="About Me"
+              width={300}
+              height={300}
+            />
+          </motion.div>
+          <motion.div
+            className="mx-auto lg:hidden block"
+            initial={{ opacity: 0, scale: 0.8 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: false }}
+          >
+            <Image
+              src={"/images/About.jpeg"}
+              alt="About Me"
+              width={300}
+              height={300}
+            />
+          </motion.div>
+          <div className="max-w-5xl mx-auto flex flex-col justify-center">
             <div>
               <p className="text-justify">
                 {t.raw("description")}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,11 +2,16 @@
 import Link from "next/link";
 import React from "react";
 import {
+  FaClock,
+  FaDiscord,
   FaFacebookF,
+  FaGithub,
   FaInstagram,
+  FaLinkedinIn,
   FaPaperPlane,
   FaSnapchat,
   FaTiktok,
+  FaX,
 } from "react-icons/fa6";
 import { motion } from "motion/react";
 import { useTranslations } from "next-intl";
@@ -21,7 +26,7 @@ function Contact({ locale }: ContactProps) {
   return (
     <section
       id="contact"
-      className="relative w-full py-12  flex flex-col items-center justify-center"
+      className="relative max-w-7xl mx-auto py-12  flex flex-col items-center justify-center"
     >
       <motion.h1
         className="text-2xl sm:text-3xl text-center text-gray-800 dark:text-white font-bold uppercase"
@@ -52,9 +57,9 @@ function Contact({ locale }: ContactProps) {
         {t("subtitle2")}
       </motion.p>
       <div className="w-full flex justify-center ">
-        <div className="w-full max-w-6xl flex flex-col lg:flex-row items-start gap-12 px-6">
+        <div className="w-full max-w-4xl flex flex-col lg:flex-row items-start  px-6">
           <motion.div
-            className="w-full lg:w-1/2 block px-8 py-10"
+            className="w-full lg:w-1/2 block px-8 lg:py-10 py-4"
             initial={{ opacity: 0, x: -50 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.6 }}
@@ -79,46 +84,102 @@ function Contact({ locale }: ContactProps) {
           </motion.div>
           {/* Right Side */}
           <motion.div
-            className="w-full lg:w-1/2 block py-10"
+            className="w-full lg:w-1/2 block px-8 lg:py-10"
             initial={{ opacity: 0, x: -50 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.6 }}
             viewport={{ once: false }}
           >
-            <h3 className="text-xl font-semibold mb-3">{t("follow")}</h3>
-            <div className="flex gap-4">
-              <Link
-                href="/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-gray-200 transition hover:bg-blue-600 bg-stone-400 dark:bg-zinc-800 rounded-xl p-2 text-white"
-              >
-                <FaFacebookF className="text-2xl" />
-              </Link>
-              <Link
-                href="/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-gray-200 transition hover:bg-rose-600 bg-stone-400 dark:bg-zinc-800 rounded-xl p-2 text-white"
-              >
-                <FaInstagram className="text-2xl" />
-              </Link>{" "}
-              <Link
-                href="/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-gray-200 transition hover:bg-yellow-600 bg-zinc-800 rounded-xl p-2 text-white"
-              >
-                <FaSnapchat className="text-2xl" />
-              </Link>{" "}
-              <Link
-                href="/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-gray-200 transition hover:bg-gray-900 bg-stone-400 dark:bg-zinc-800 rounded-xl p-2 text-white"
-              >
-                <FaTiktok className="text-2xl" />
-              </Link>
+            <div className="flex flex-col mb-6">
+              <h3 className="text-xl font-semibold mb-3">{t("follow")}</h3>
+              <div className="relative z-10 flex flex-wrap gap-6 lg:text-4xl text-2xl justify-center items-center py-2">
+                <Link
+                  href="https://github.com/TJ745"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub />
+                </Link>
+                <Link
+                  href="https://linkedin.com/in/talha-jamil-937b3b32"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedinIn />
+                </Link>
+                <Link
+                  href="https://instagram.com/t_e_e.j_a_y"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaInstagram />
+                </Link>
+                <Link
+                  href="https://snapchat.com/add/te3_j4y"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaSnapchat />
+                </Link>
+                <Link
+                  href="https://discord.gg/teejay745"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaDiscord />
+                </Link>
+                <Link
+                  href="https://x.com/yourtwitter"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaX />
+                </Link>
+              </div>
+              <div className="flex gap-3">
+                {/* <Link
+                  href="/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-gray-200 transition hover:bg-blue-600 bg-stone-400 dark:bg-zinc-800 rounded-xl p-2 text-white"
+                >
+                  <FaFacebookF className="text-2xl" />
+                </Link>
+                <Link
+                  href="/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-gray-200 transition hover:bg-rose-600 bg-stone-400 dark:bg-zinc-800 rounded-xl p-2 text-white"
+                >
+                  <FaInstagram className="text-2xl" />
+                </Link>{" "}
+                <Link
+                  href="/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-gray-200 transition hover:bg-yellow-600 bg-zinc-800 rounded-xl p-2 text-white"
+                >
+                  <FaSnapchat className="text-2xl" />
+                </Link>{" "}
+                <Link
+                  href="/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-gray-200 transition hover:bg-gray-900 bg-stone-400 dark:bg-zinc-800 rounded-xl p-2 text-white"
+                >
+                  <FaTiktok className="text-2xl" />
+                </Link> */}
+              </div>
+            </div>
+            <div className="w-full border dark:border-white p-4 rounded-2xl">
+              <div className="flex items-center mb-2">
+                <FaClock className="inline-block mr-2 text-xl text-gray-800 dark:text-white" />
+                <h1>Availability</h1>
+              </div>
+              <p>
+                I response within 24 hours. Open to freelance opportunities and
+                collaborations.
+              </p>
             </div>
           </motion.div>
         </div>
